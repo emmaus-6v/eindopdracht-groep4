1ce973cebe3eb232c4c4344e800956b0e6ff7ca9
JasmijnvdTempel <70636162+JasmijnvdTempel@users.noreply.github.com>
2021-03-02 17:07:00 +0100

    Update knikkerbaan.ino

--- knikkerbaan/knikkerbaan.ino
+++ knikkerbaan/knikkerbaan.ino
@@ -1,4 +1,4 @@
-const int ledPin =  LED_BUILTIN;
+/* const int ledPin =  LED_BUILTIN;
 
 // globale variabelen
 int ledState = LOW;               // status van de LED
@@ -32,3 +32,60 @@ void loop() {
     digitalWrite(ledPin, ledState);
   }
 }
+
+*/
+// sensor
+int pinLaser = 7;
+int pinReciever = 6;
+
+// variabelen van lampjes 
+int rood = ..;
+int geel = ..;
+int blauw = ..;
+int groen = ..;
+
+// variabele voor tellen van hoeveel balletjes voorbij komen
+int aantalBalletjes = 0;
+
+void setup () {
+  // aangeven of variabele een OUTPUT of INPUT is
+  pinMode (pinLaser, OUTPUT);
+  pinMode (pinReceiver, INPUT);
+  pinMode (rood, OUTPUT);
+  pinMode (geel, OUTPUT);
+  pinMode (blauw, OUTPUT);
+  pinMode (groen, OUTPUT);
+  digitalWrite(pinLaser, HIGH);
+  Serial.begin (9600);
+}
+
+void loop () {
+  int value = digitalRead (pinReceiver);
+  Serial.println (value);
+  
+  // als er een balletje voorbij komt, receiver ontvangt niets
+  if (value == LOW) {
+    digitalWrite (rood, HIGH);
+    digitalWrite (geel, HIGH);
+    digitalWrite (blauw, HIGH);
+    digitalWrite (groen, HIGH);
+  }
+  
+  // als er geen balletje voorbij komt, receiver ontvangt laserstraal
+  if (value == HIGH) {
+    digitalWrite (rood, LOW);
+    digitalWrite (geel, LOW);
+    digitalWrite (blauw, LOW);
+    digitalWrite (groen, LOW);
+  }
+    
+  /* als er een balletje voorbij komt, receiver ontvangt niets, 
+  tellen we dit balletje op bij het aantalBalletjes, zo kunnen we zien hoeveel balletjes er langs zijn geweest. 
+  eigenlijk wilde we dit op de widget weergeven, maar dit lukte niet, dus geven we het weer in de Serial Monitor */
+  if (value == LOW) {
+    aantalBalletjes = aantalBalletjes + 1;
+    Serial.println(aantalBalletjes);
+  }
+}
+
+
